fcc391824b5278cdd36a15f930300fcad6412d52
ruchika-13 <87175244+ruchika-13@users.noreply.github.com>
2024-12-16 16:40:04 +0530

    Update lcdtest.py

--- lcdtest.py
+++ lcdtest.py
@@ -1,16 +1,13 @@
 import time
 import board
-import digitalio
-import adafruit_character_lcd.character_lcd_i2c as character_lcd
 import busio
+import adafruit_character_lcd.character_lcd_i2c as character_lcd
 
 # I2C setup
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# LCD 1602 setup (adjust the address if necessary)
-lcd = CharLCD(i2c_expander='PCF8574', address=0x2d, port=1, cols=16, rows=2, dotsize=8)
-lcd.clear()
-
+# LCD 1602 setup with address 0x2D (adjust the address if necessary)
+lcd = character_lcd.Character_LCD_I2C(i2c, 16, 2, address=0x2D)  # 16x2 LCD with address 0x2D
 
 # Clear the LCD screen
 lcd.clear()
