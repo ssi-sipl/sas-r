9e568f35127115b35af2a8f296c19a0fffe51a82
ruchika-13 <87175244+ruchika-13@users.noreply.github.com>
2024-12-16 16:12:06 +0530

    Update lcdtest.py

--- lcdtest.py
+++ lcdtest.py
@@ -1,14 +1,6 @@
-import time
-from LCD import LCD
+from RPLCD.i2c import CharLCD
 
-# Initialize the LCD with specific parameters: Raspberry Pi revision, I2C address, and backlight status
-lcd = LCD(2, 0x2d, True)  # Using Raspberry Pi revision 2, I2C address 0x27, backlight enabled
-
-# Display messages on the LCD
-lcd.message("Hello World!", 1)        # Display 'Hello World!' on line 1
-
-# Keep the messages displayed for 5 seconds
-time.sleep(5)
-
-# Clear the LCD display
+lcd = CharLCD(i2c_expander='PCF8574', address=0x2d, port=1, cols=16, rows=2, dotsize=8)
 lcd.clear()
+
+lcd.write_string('Hello, World!')
