d19c9aa6c88304bde2c7e4bc90cc5ebb44faa3ec
ruchika-13 <87175244+ruchika-13@users.noreply.github.com>
2024-12-24 10:59:29 +0530

    Update fingerprinttest.py

--- fingerprinttest.py
+++ fingerprinttest.py
@@ -12,7 +12,7 @@ led.direction = Direction.OUTPUT
 
 uart = busio.UART(board.TX, board.RX, baudrate=57600)
 
-If using with a computer such as Linux/RaspberryPi, Mac, Windows with USB/serial converter:
+#If using with a computer such as Linux/RaspberryPi, Mac, Windows with USB/serial converter:
 import serial
 uart = serial.Serial("/dev/ttyUSB0", baudrate=57600, timeout=1)
 
