5e59e92c1de0088925c321ea666a58e1dc594307
ruchika-13 <87175244+ruchika-13@users.noreply.github.com>
2025-01-06 12:31:41 +0530

    Update logtestnew.py

--- logtestnew.py
+++ logtestnew.py
@@ -30,14 +30,14 @@ def hash_fingerprint_template(template):
     return sha256_hash
 
 
-#def clear_fingerprint_buffer():
+def clear_fingerprint_buffer():
     """
-#    Clears the fingerprint sensor buffer slots.
+    Clears the fingerprint sensor buffer slots.
     """
-#   if finger.empty_library() == adafruit_fingerprint.OK:
-#        print("Fingerprint buffer cleared successfully.")
-#    else:
-#        print("Failed to clear fingerprint buffer.")
+   if finger.empty_library() == adafruit_fingerprint.OK:
+        print("Fingerprint buffer cleared successfully.")
+    else:
+        print("Failed to clear fingerprint buffer.")
 
 
 
@@ -59,8 +59,8 @@ def send_post_request(endpoint, data):
 
 def process_fingerprint():
     
-    #print("Clearing fingerprint buffer...")
-    #clear_fingerprint_buffer()  # Clear buffer before starting a new search
+    print("Clearing fingerprint buffer...")
+    clear_fingerprint_buffer()  # Clear buffer before starting a new search
 
     
     """Capture fingerprint, retrieve template, and send to backend."""
