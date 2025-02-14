0d1333077c568fb468c969b39eaa2e955b9c87c6
abhinnvyas <abhinnvyas2@gmail.com>
2025-02-14 17:49:52 +0530

    minor fixes

--- main/enrollment.py
+++ main/enrollment.py
@@ -6,7 +6,7 @@ import adafruit_fingerprint
 from library import AttendanceSystemManager
 
 # Setup serial connection for the fingerprint sensor
-uart = serial.Serial("/dev/ttyAMA0", baudrate=57600, timeout=1)
+uart = serial.Serial("/dev/ttyS0", baudrate=57600, timeout=1)
 time.sleep(2)
 finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
 
